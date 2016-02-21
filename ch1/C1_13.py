825ecdbebcc2e1ef35a5d9ae230e4d534b87beda
xhuang281 <xhuang281@gmail.com>
2016-02-21 00:32:54 -0500

    Update C1_13.py

--- ch1/C1_13.py
+++ ch1/C1_13.py
@@ -2,6 +2,7 @@
 # Author: TianJun
 # Mail: find_my_way@foxmail.com
 # Created Time: 2013/12/16 11:00:31
+
 def myRevers(data):
     for i in range(len(data) // 2):
         data[i], data[-i - 1] = data[-i - 1], data[i]
